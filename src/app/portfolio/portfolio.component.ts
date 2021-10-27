c7253cc1f269de7cf519b7dc460a6a13bc5429f9
LakshmiVSeelam <lakshmivseelam@gmail.com>
2021-10-27 13:04:33 +0530

    portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,10 +10,12 @@ import $ from '../../assets/js/vendor/jquery.js'
 export class PortfolioComponent implements OnInit {
   active_cat = 'all'
   categories = [
-    {'id': 'all', 'name': 'Our Creations'},
-    {'id': 'website', 'name': 'Website Designs'},
+    // {'id': 'all', 'name': 'Our Creations'},
     {'id': 'graphic', 'name': 'Graphic Designs'},
-    {'id': 'videos', 'name': '3D Animation'}
+    {'id': 'videos', 'name': 'After Effects'},
+    {'id': 'website', 'name': 'Clientele'}
+    
+    
   ]
 
   videos = []
@@ -23,6 +25,7 @@ export class PortfolioComponent implements OnInit {
   graphic_img_arr = []
   website_img_arr = []
   final_img_arr = []
+
 
   constructor() { 
     
@@ -34,13 +37,18 @@ export class PortfolioComponent implements OnInit {
     let videos_url = 'assets/data/video_data.json'
     let website_url = 'assets/data/website_img_data.json'
     
-    $.when(
-        $.getJSON(graphics_url),
-        $.getJSON(website_url),
-        $.getJSON(videos_url)
-    ).done(function(res1, res2, res3) {
-      self.final_img_arr = res1[0].concat(res2[0])
-      self.videos = res3[0]
-    });
+    // $.when(
+    //     $.getJSON(graphics_url),
+    //     $.getJSON(website_url),
+    //     $.getJSON(videos_url)
+    // ).done(function(res1, res2, res3) {
+    //   self.final_img_arr = res1[0].concat(res2[0])
+    //   self.videos = res3[0]
+    // });
+
+    $.getJSON('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCYn6oNDCGVXfd9Mkg3umSSw&maxResults=25&key=AIzaSyAj8h3s8_Lphr0eo1rN80KYPjWZAbD9L0Q').done(res => {
+      self.videos = res.items
+      console.log(res)
+    })
   }
 }
